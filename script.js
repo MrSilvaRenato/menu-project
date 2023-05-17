938871837af82cbae3bb3d4c60905247e846f868
Renato <37394601+MrSilvaRenato@users.noreply.github.com>
2023-05-18 08:01:47 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    if (menu.classList.contains('menu-open')) {
+        menu.classList.remove('menu-open');
+    } else {
+        menu.classList.add('menu-open');
+    }
+}
